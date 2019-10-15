21960ce53ab5d1bdae82dd401d7bd15b2b1f6826
Brandon Alejandro Mosqueda González <brandon16mg@gmail.com>
2019-10-15 17:16:31 -0500

    First commit, project initialization

---
+++ datos.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `datos` (
+  `id` int(8) unsigned NOT NULL auto_increment,
+  `nombre` varchar(70) default NULL,
+  `direccion` varchar(120) default NULL,
+  `fechaCreacion` timestamp NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+INSERT INTO datos(nombre, direccion, fechaCreacion)
+VALUES('Brandon Mosqueda', 'Semirecumbent #23, Nepal', '2015-12-09'),
+      ('Alejandro González', 'Swindle #70, Jamaica', '2020-05-03'),
+      ('Floyd Giesbrecht', 'Employed #38, Uzbekistan', '2028-01-17');
