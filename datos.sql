8811736fa934bc7fd7818cc3089918eac9a89540
Brandon Alejandro Mosqueda González <brandon16mg@gmail.com>
2019-10-15 19:16:41 -0500

    #2 - Phone field added to database and in formularies of update and create person

--- datos.sql
+++ datos.sql
@@ -10,3 +10,5 @@ INSERT INTO datos(nombre, direccion, fechaCreacion)
 VALUES('Brandon Mosqueda', 'Semirecumbent #23, Nepal', '2015-12-09'),
       ('Alejandro González', 'Swindle #70, Jamaica', '2020-05-03'),
       ('Floyd Giesbrecht', 'Employed #38, Uzbekistan', '2028-01-17');
+
+ALTER TABLE datos ADD COLUMN telefono VARCHAR(10) DEFAULT NULL AFTER direccion; 
