09c9f450beb566b39aa48257b6cca7e630b33859
Victor Medina <vmedina@ucol.mx>
2019-10-30 00:34:55 -0600

    Ajuste de prueba #1

--- cypress/integration/test-delete.spec.js
+++ cypress/integration/test-delete.spec.js
@@ -5,12 +5,6 @@ describe('Prueba del Issue ',function() {
     })  
   
     it('Borrar el primero de directorio', function() {    
-      //cy.get('#q').click() 
-      //cy.get('#q')
-      //  .type('plataforma educ').should('have.value', 'plataforma educ')
-       //cy.get('.form-search-ucol').submit() 
-       //cy.contains('Borrar').click() 
-       //cy.get('.btn-danger').click()
        cy.get(':nth-child(1) > :nth-child(5) > .btn-danger').click()
     })  
       
