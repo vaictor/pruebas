98c033b0668476802cdc18c9419e8853bd021cdc
Victor Medina <vmedina@ucol.mx>
2019-10-30 00:07:05 -0600

    Ajuste de librerias de boostrap

--- includes/views/index.view.php
+++ includes/views/index.view.php
@@ -2,8 +2,9 @@
 
 	
 	<link href="<?php echo $url;?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<script src="<?php echo $url;?>assets/js/jquery.js"></script>
-	<script src="<?php echo $url;?>assets/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 	<script src="<?php echo $url;?>assets/js/tooltip.js"></script>
 	<script src="<?php echo $url;?>assets/js/angular.js"></script>
 
